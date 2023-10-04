50fc026b8c2b1d565934bdcbb9e720961bfacd0f
Rokia Darwish <94113862+Rokia-DSC@users.noreply.github.com>
2023-10-05 01:29:08 +0300

    done

--- lib/main.dart
+++ lib/main.dart
@@ -67,7 +67,9 @@ class _StoryPageState extends State<StoryPage> {
                   onPressed: () {
                     //Choice 1 made by user.
                     //Step 18 - Call the nextStory() method from storyBrain and pass the number 1 as the choice made by the user.
-                    storyBrain.nextStory(1);
+                    setState(() {
+                      storyBrain.nextStory(1);
+                    });
                   },
                   style: ButtonStyle(
                     backgroundColor:
@@ -87,20 +89,26 @@ class _StoryPageState extends State<StoryPage> {
                 flex: 2,
                 //Step 26 - Use a Flutter Visibility Widget to wrap this FlatButton.
                 //Step 28 - Set the "visible" property of the Visibility Widget to equal the output from the buttonShouldBeVisible() method in the storyBrain.
-                child: TextButton(
-                  onPressed: () {
-                    //Choice 2 made by user.
-                    // Step 19 - Call the nextStory() method from storyBrain and pass the number 2 as the choice made by the user.
-                    storyBrain.nextStory(2);
-                  },
-                  style: ButtonStyle(
-                    backgroundColor:
-                        MaterialStateProperty.all<Color>(Colors.blue),
-                  ),
-                  child: Text(
-                    //Step 14 - Use the storyBrain to get the text for choice 2.
-                    storyBrain.getChoice2()!,
-                    style: const TextStyle(fontSize: 20.0, color: Colors.white),
+                child: Visibility(
+                  visible: storyBrain.buttonShouldBeVisible()!,
+                  child: TextButton(
+                    onPressed: () {
+                      //Choice 2 made by user.
+                      // Step 19 - Call the nextStory() method from storyBrain and pass the number 2 as the choice made by the user.
+                      setState(() {
+                        storyBrain.nextStory(2);
+                      });
+                    },
+                    style: ButtonStyle(
+                      backgroundColor:
+                          MaterialStateProperty.all<Color>(Colors.blue),
+                    ),
+                    child: Text(
+                      //Step 14 - Use the storyBrain to get the text for choice 2.
+                      storyBrain.getChoice2()!,
+                      style:
+                          const TextStyle(fontSize: 20.0, color: Colors.white),
+                    ),
                   ),
                 ),
               ),
